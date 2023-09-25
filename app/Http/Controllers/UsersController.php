1e5099ea6800872c8b3a448e7e0fc20ad88cbadb
Danis staff <dstaff@bladex.com>
2023-09-25 15:44:46 -0500

    rollback user pagiantion

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
             'users' => Auth::user()->account->users()
                 ->orderByName()
                 ->filter(Request::only('search', 'role', 'trashed'))
-                ->paginate(10)
+                ->get()
                 ->transform(fn ($user) => [
                     'id' => $user->id,
                     'name' => $user->name,
